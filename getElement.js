9a7991b324447ad6845c0976c9a8a2d80652ac85
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2022-06-09 08:55:53 +0300

    update with cucumber expressions

--- getElement.js
+++
@@ -1,6 +0,0 @@
-const { po } = require('@cucumber-e2e/po2');
-const memory = require('@cucumber-e2e/memory2');
-
-module.exports = async function (alias) {
-    return po.getElement(memory.getValue(alias));
-}
